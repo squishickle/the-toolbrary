8a7415ac96de98793d023fc5692a0afb7c4fb0bf
Gin Atkins <vcatkins@gmail.com>
2019-11-19 14:47:02 +1100

    Added name and address to sign-up fields and presence validation for both on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   has_many :rentals, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, :address, presence: true
 end
