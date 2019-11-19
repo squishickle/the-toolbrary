50b005820224ab856c48c4d241d6b37583c08c20
Allen Liao <a@tzukuri.com>
2019-11-19 11:27:25 +1100

    added associations and validations

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,22 @@
 class Rental < ApplicationRecord
+  validate :no_overlaps
+  validates :tool_id, presence: true
+  validates :user_id, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
+  belongs_to :tool
+  belongs_to :user
+
+  # Check if a given interval overlaps this interval
+  private
+
+  def no_overlaps
+    rentals = Rental.all.delete(self)
+    rentals.each do |rental|
+      if start_date <= rental.end_date && rental.start_date <= end_date
+        errors.add(:base, :date_overlaps, message: "booking date not available")
+      end
+    end
+  end
 end
