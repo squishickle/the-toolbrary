6112a5c48706955f5919018a7e58f1d16308ce7f
joseph lam <jvlam318@gmail.com>
2019-11-20 16:45:49 +1100

    push seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Tool.destroy_all
 Rental.destroy_all
 
 2.times do
   User.create!(name: Faker::FunnyName.name, address: Faker::Address.full_address, email: Faker::Internet.email, password: "123456")
 end
 
-Tool.create!(name: "CIRCULAR SAW",
-            description: "The smallest professional universal saw.",
-            image: "https://www.totaltools.com.au/media/catalog/product/cache/ecd051e9670bd57df35c8f0b122d8aea/1/3/133700-bosch-saw-circular-85mm-12v-skin-only-hero2-06016a1001.jpg",
-            condition: true,
-            user_id: 11,
-            availability_status: true,
-            price: 10,
-            brand: "Bosch",
-            condition_detail: "As new",
-            tech_specs: "Voltage  12V, Barcode 3165140755849, Part No 06016A1001, Product Length (mm)",
-            parts_list: "LED Light",
-            category: "Carpentry")
-
 Tool.create!(name: "JACK TROLLEY",
             description: "Blue jack trolley. Great for jacking stuff.",
             image: "https://www.tlib.brunswicktoollibrary.org/TL/toolPx/px/30742.jpg",
             condition: true,
-            user_id: 12,
+            user_id: 3,
             availability_status: true,
             price: 15,
             brand: "Orcon",
@@ -44,7 +29,7 @@ Tool.create!(name: "ALUMINIUM A-FRAME LADDER",
             description: "A-FRAME ladder, perfect for peering over fences.",
             image: "https://www.tlib.brunswicktoollibrary.org/TL/toolPx/px/30719.jpg",
             condition: true,
-            user_id: 11,
+            user_id: 4,
             availability_status: true,
             price: 18,
             brand: "Bailey",
@@ -52,3 +37,16 @@ Tool.create!(name: "ALUMINIUM A-FRAME LADDER",
             tech_specs: "Product Length (1800mm)",
             parts_list: "",
             category: "Ladders")
+
+Tool.create!(name: "Steam Cleaner",
+            description: "Sturdy steam cleaner to remove those stubborn blood stains.",
+            image: "https://images.unsplash.com/photo-1527515637462-cff94eecc1ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+            condition: true,
+            user_id: 2,
+            availability_status: true,
+            price: 18,
+            brand: "Mile",
+            condition_detail: "As new",
+            tech_specs: "Product Length (1800mm)",
+            parts_list: "",
+            category: "Cleaning")
