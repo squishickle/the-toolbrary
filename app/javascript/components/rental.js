b2998e5c47c44ef2f7b871974d5ef930c01c5b7b
Allen Liao <a@tzukuri.com>
2019-11-20 14:23:18 +1100

    added logic to dynamically change price when dates selected

--- app/javascript/components/rental.js
+++ app/javascript/components/rental.js
@@ -1,13 +1,11 @@
 import flatpickr from 'flatpickr';
 
 const toggleDateInputs = function() {
-  console.log("hello");
   const startDateInput = document.getElementById('rental_start_date');
   const endDateInput = document.getElementById('rental_end_date');
 
   if (startDateInput && endDateInput) {
     const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
-    console.log(unvailableDates);
     flatpickr(startDateInput, {
       minDate: 'today',
       dateFormat: 'Y-m-d',
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,7 @@ import "bootstrap";
 import 'flatpickr/dist/flatpickr.css';
 
 import { toggleDateInputs } from '../components/rental';
+import { changePrice } from '../components/price';
 
 toggleDateInputs();
+changePrice();
