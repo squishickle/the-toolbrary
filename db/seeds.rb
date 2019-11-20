bf48bad91f15b358fbc03afad9c9cba9fd3e7a7d
joseph lam <jvlam318@gmail.com>
2019-11-20 13:47:01 +1100

    add tool-card updated index page for tools added title to header add styling for tool-cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,41 @@ Rental.destroy_all
   User.create!(name: Faker::FunnyName.name, address: Faker::Address.full_address, email: Faker::Internet.email, password: "123456")
 end
 
-Tool.create!(name: "12V 85MM CIRCULAR SAW",
+Tool.create!(name: "CIRCULAR SAW",
             description: "The smallest professional universal saw.",
             image: "https://www.totaltools.com.au/media/catalog/product/cache/ecd051e9670bd57df35c8f0b122d8aea/1/3/133700-bosch-saw-circular-85mm-12v-skin-only-hero2-06016a1001.jpg",
             condition: true,
             user_id: 1,
             availability_status: true,
-            price: 5,
+            price: 10,
             brand: "Bosch",
             condition_detail: "As new",
             tech_specs: "Voltage  12V, Barcode 3165140755849, Part No 06016A1001, Product Length (mm)",
             parts_list: "LED Light",
             category: "Carpentry")
+
+Tool.create!(name: "JACK TROLLEY",
+            description: "Blue jack trolley. Great for jacking stuff.",
+            image: "https://www.tlib.brunswicktoollibrary.org/TL/toolPx/px/30742.jpg",
+            condition: true,
+            user_id: 2,
+            availability_status: true,
+            price: 15,
+            brand: "Orcon",
+            condition_detail: "As new",
+            tech_specs: "2000kg, Barcode 3165189955849, Part No 68616A1001, Product Length (mm)",
+            parts_list: "",
+            category: "automotive")
+
+Tool.create!(name: "ALUMINIUM A-FRAME LADDER",
+            description: "A-FRAME ladder, perfect for peering over fences.",
+            image: "https://www.tlib.brunswicktoollibrary.org/TL/toolPx/px/30719.jpg",
+            condition: true,
+            user_id: 1,
+            availability_status: true,
+            price: 18,
+            brand: "Bailey",
+            condition_detail: "As new",
+            tech_specs: "Product Length (1800mm)",
+            parts_list: "",
+            category: "Ladders")
