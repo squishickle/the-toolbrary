25a72d86f6b95809835aa19e2ef01af1ed3076c7
Gin Atkins <vcatkins@gmail.com>
2019-11-21 16:03:24 +1100

    added map and marker for tool address

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,9 @@ import { changePrice } from '../components/price';
 
 toggleDateInputs();
 changePrice();
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,46 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Rental.destroy_all
+Tool.destroy_all
+User.destroy_all
 
-2.times do
-  User.create!(name: Faker::FunnyName.name, address: Faker::Address.full_address, email: Faker::Internet.email, password: "123456")
-end
+
+# 2.times do
+#   User.create!(name: Faker::FunnyName.name, address: Faker::Address.full_address, email: Faker::Internet.email, password: "123456")
+# end
+
+user = User.create!(email: 'user@user.com', password: '123456', name: Faker::FunnyName.name, address: '50 Bourke St Melbourne Australia')
+user1 = User.create!(email: 'pickle@user.com', password: '123456', name: Faker::FunnyName.name, address: '500 Collins St Melbourne Australia')
 
 Tool.create!(name: "JACK TROLLEY",
             description: "Blue jack trolley. Great for jacking stuff.",
             image: "https://www.tlib.brunswicktoollibrary.org/TL/toolPx/px/30742.jpg",
             condition: true,
-            user_id: 3,
+            user: user,
             availability_status: true,
             price: 15,
             brand: "Orcon",
             condition_detail: "As new",
             tech_specs: "2000kg, Barcode 3165189955849, Part No 68616A1001, Product Length (mm)",
-            parts_list: "",
+            parts_list: "some parts",
             category: "automotive")
 
 Tool.create!(name: "ALUMINIUM A-FRAME LADDER",
             description: "A-FRAME ladder, perfect for peering over fences.",
             image: "https://www.tlib.brunswicktoollibrary.org/TL/toolPx/px/30719.jpg",
             condition: true,
-            user_id: 4,
+            user: user1,
             availability_status: true,
             price: 18,
             brand: "Bailey",
             condition_detail: "As new",
             tech_specs: "Product Length (1800mm)",
-            parts_list: "",
+            parts_list: "some parts",
             category: "Ladders")
-
-Tool.create!(name: "Steam Cleaner",
-            description: "Sturdy steam cleaner to remove those stubborn blood stains.",
-            image: "https://images.unsplash.com/photo-1527515637462-cff94eecc1ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            condition: true,
-            user_id: 2,
-            availability_status: true,
-            price: 18,
-            brand: "Mile",
-            condition_detail: "As new",
-            tech_specs: "Product Length (1800mm)",
-            parts_list: "",
-            category: "Cleaning")
