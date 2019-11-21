7abbff5260bf6229963fe9fb0bb73cd46c91b213
Allen Liao <a@tzukuri.com>
2019-11-21 11:13:16 +1100

    hid cursor in typer when searchbox focuses

--- app/javascript/components/search.js
+++ app/javascript/components/search.js
@@ -1,10 +1,9 @@
 const hideTypedText = function() {
   console.log("hello");
-  const typedText = document.querySelector('.typer');
+  const typedText = document.querySelector('.typer-text');
   const searchBar = document.querySelector('.form-control');
   // apply above function to event listeners endDateInput and startDateInput
   searchBar.addEventListener('focus', (event) => {
-    console.log("hello");
     typedText.style.display = "none";
   });
 };
