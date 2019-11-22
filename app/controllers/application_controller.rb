b6e59dde4df0b559723533537d27fcdf0192a94d
joseph lam <jvlam318@gmail.com>
2019-11-22 16:48:58 +1100

    add edit photo feature to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :photo, :password, :password_confirmation, :current_password])
-    update_attrs = [:password, :password_confirmation, :current_password]
+    update_attrs = [:password, :password_confirmation, :current_password, :photo]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
   end
 
